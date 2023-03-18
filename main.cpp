292b6952cbcc04a040bda0de8abc68dfcf1830e1
twofacedshark <twofacedshark@gmail.com>
2023-03-18 13:21:41 -0500

    Separate input from ShortList.txt based on length

--- main.cpp
+++ main.cpp
@@ -1,47 +1,103 @@
-/**
-
-1 = ---
-2 = abc
-3 = def
-4 = ghi
-5 = jkl
-6 = mno
-7 = pqrs
-8 = tuv
-9 = wxyz
-0 = ---
-
-**/
-
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cctype>   // allows access to functions for chars
+#include <fstream>  // allows i/o stream
 using namespace std;
 
 
 // functions --
 bool IsPhoneValid(string phoneNum);
 
+// - - - - - - - - - - - - - - - - - -
 
 int main() {
     // collect input phone number (###-####)
     string phoneNum;
     cin >> phoneNum;
 
-                                                                    // TODO: read from ShortList.txt
+    // reads from text file "ShortList.txt"
+    ifstream fin("ShortList.txt");
+    // outputs to text file "Mnemonics.txt"
+    ofstream fout("output.txt");
+
+    bool validity;
+    validity = IsPhoneValid(phoneNum);
 
     // checks if phoneNum is valid
-    if (IsPhoneValid(phoneNum) == false) {
+    if (validity == false) {
         cout << "Invalid phone number" << endl;
     }
 
     else {
         cout << "Valid!" << endl;                                   // FIXME: delete before submission
     }
+
+
+
+
+    // misc variables
+    string line;
+
+    // declare separate vectors for words of length 3, 4, and 7
+    vector<string> output3(0);
+    vector<string> output4(0);
+    vector<string> output7(0);
+
+    // separate phoneNum into three strings (###-#### -> ###, ####, and #######)
+    string phone1;
+    string phone2;
+    string phoneTot;
+
+    phone1 = phoneNum.substr(0,3);      // output3
+    phone2 = phoneNum.substr(4,7);      // output4
+    phoneTot = phone1 + phone2;         // output7
+
+    // two dimension array of letters:numbers
+    char num2Letters[8][5] = {
+        {'2', 'a', 'b', 'c'},       // 0
+        {'3', 'd', 'e', 'f'},       // 1
+        {'4', 'g', 'h', 'i'},       // 2
+        {'5', 'j', 'k', 'l'},       // 3
+        {'6', 'm', 'n', 'o'},       // 4
+        {'7', 'p', 'q', 'r', 's'},  // 5
+        {'8', 't', 'u', 'v'},       // 6
+        {'9', 'w', 'x', 'y', 'z'}   // 7
+        // 0   1    2    3    4
+    };
+
+    // while ShortList.txt inputs a line
+    while (fin >> line) {
+
+        // if line is 3 characters
+        if (line.size() == 3) {
+            output3.push_back(line);
+        }
+
+        // if line is 4 characters
+        if (line.size() == 4) {
+            output4.push_back(line);
+        }
+
+        // if line is 7 characters
+        if (line.size() == 7) {
+            output7.push_back(line);
+        }
+    }
+
+    // write each word in the final array into output.txt
+
+
+
+    // closes "ShortList.txt"
+    fin.close();
+    // closes "output.txt"
+    fout.close();
     
     return 0;
 }
 
+// - - - - - - - - - - - - - - - - - -
 
 // functions --
 bool IsPhoneValid(string phoneNum) {
